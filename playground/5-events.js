f69343a172ff3f61a89921a8e2d9075fa32749c2
Victor Teixeira <teixeira.victorv@gmail.com>
2023-11-30 18:58:43 +0900

    Update comment

--- playground/5-events.js
+++ playground/5-events.js
@@ -15,7 +15,8 @@ eventEmitter.on(EVENT_NAME, (data) => {
   console.log(data, ' - second');
 });
 
-// will follow the created listener order:
+// will follow the created listener order
+// also, events are synchronous and blocking
 eventEmitter.emit(EVENT_NAME, 'Emitted Statement');
 
 console.log('Statement B');
