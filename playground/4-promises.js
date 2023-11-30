1b88780a37379ea02b1eb87f891d0e72784171a4
Victor Teixeira <teixeira.victorv@gmail.com>
2023-11-30 18:58:30 +0900

    Promise chaining tests

--- playground/4-promises.js
+++ playground/4-promises.js
@@ -11,3 +11,21 @@ doSomethingPromise.then((result) => {
 }).catch((error) => {
   console.log('Error: ', error)
 })
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b)
+    }, 3000)
+  })
+}
+
+// chaining
+add(1, 1).then((sum) => {
+  console.log(sum)
+  return add(sum, 4)
+}).then((sum2) => {
+  console.log(sum2)
+}).catch((err) => {
+  console.log(err)
+})
